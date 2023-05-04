2a88d29ba04ac93379f3251d00245da0ec948366
markomilos216 <127882026+markomilos216@users.noreply.github.com>
2023-05-04 11:05:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function matematickeOperacie(a = 4 , b = 2){
+    console.log("Hodnoty parametrov sú " + a + " a " + b);
+    console.log("Vykonávam matematické operácie.");
+    console.log(a + " + " + b + " = " + (a + b));
+    console.log(a + " - " + b + " = " + (a - b));
+    console.log(a + " * " + b + " = " + (a * b));
+    console.log(a + " / " + b + " = " + (a / b));
+}
+
+matematickeOperacie();
+matematickeOperacie(10, 5);
